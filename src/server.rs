adc079e765b6a775b17dba60406f97e5790bca5a
poffo <ltkpoffo@gmail.com>
2021-07-09 20:29:04 -0300

    Server: Move module to only one file, server.rs

--- src/server.rs
+++ src/server.rs
@@ -5,8 +5,6 @@ use crate::packet::{Packet, Unauthenticaded};
 use crate::protocol::ServerAuthenticationProtocol;
 use crate::remote_connection::{ClientId, ConnectionConfig, NetworkInfo, RemoteConnection};
 
-use super::ServerConfig;
-
 use log::{debug, error, info};
 
 use std::collections::HashMap;
@@ -14,13 +12,35 @@ use std::collections::VecDeque;
 use std::io;
 use std::net::{SocketAddr, UdpSocket};
 
+pub struct ServerConfig {
+    pub max_clients: usize,
+    pub max_payload_size: usize,
+}
+
+impl ServerConfig {
+    pub fn new(max_clients: usize, max_payload_size: usize) -> Self {
+        Self {
+            max_clients,
+            max_payload_size,
+        }
+    }
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            max_clients: 16,
+            max_payload_size: 8 * 1024,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum ServerEvent {
     ClientConnected(ClientId),
     ClientDisconnected(ClientId),
 }
 
-// TODO: add internal buffer?
 pub struct Server<P: ServerAuthenticationProtocol> {
     config: ServerConfig,
     socket: UdpSocket,
--- src/server/mod.rs
+++
@@ -1,5 +0,0 @@
-mod server_config;
-mod server_impl;
-
-pub use server_config::ServerConfig;
-pub use server_impl::{Server, ServerEvent};
