118ce5cce9673c08b85ff42e182d8ad87eff9dea
poffo <ltkpoffo@gmail.com>
2022-04-13 03:05:01 -0300

    BevyRenet: add simple boxes example with movement to verify plugin.

---
+++ bevy_renet/examples/simple.rs
@@ -0,0 +1,254 @@
+use bevy::prelude::*;
+use bevy_renet::{
+    renet::{ConnectToken, RenetClient, RenetConnectionConfig, RenetServer, ServerConfig, ServerEvent, NETCODE_KEY_BYTES},
+    run_if_client_conected, RenetClientPlugin, RenetServerPlugin,
+};
+
+use bincode;
+use std::time::SystemTime;
+use std::{collections::HashMap, net::UdpSocket};
+
+use serde::{Deserialize, Serialize};
+
+const PRIVATE_KEY: &[u8; NETCODE_KEY_BYTES] = b"an example very very secret key."; // 32-bytes
+const PROTOCOL_ID: u64 = 7;
+
+const PLAYER_MOVE_SPEED: f32 = 1.0;
+
+#[derive(Debug, Default, Serialize, Deserialize, Component)]
+struct PlayerInput {
+    up: bool,
+    down: bool,
+    left: bool,
+    right: bool,
+}
+
+#[derive(Debug, Component)]
+struct Player {
+    id: u64,
+}
+
+#[derive(Debug, Default)]
+struct Lobby {
+    players: HashMap<u64, Entity>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Component)]
+enum ServerMessages {
+    PlayerConnected { id: u64 },
+    PlayerDisconnected { id: u64 },
+}
+
+fn new_renet_client() -> RenetClient {
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let connection_config = RenetConnectionConfig::default();
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let client_id = current_time.as_millis() as u64;
+    let connect_token =
+        ConnectToken::generate(current_time, PROTOCOL_ID, 300, client_id, 15, vec![server_addr], None, PRIVATE_KEY).unwrap();
+    RenetClient::new(current_time, socket, client_id, connect_token, connection_config).unwrap()
+}
+
+fn new_renet_server() -> RenetServer {
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let connection_config = RenetConnectionConfig::default();
+    let server_config = ServerConfig::new(64, PROTOCOL_ID, server_addr, *PRIVATE_KEY);
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    RenetServer::new(current_time, server_config, connection_config, socket).unwrap()
+}
+
+fn main() {
+    println!("Usage: server [SERVER_PORT] or client [SERVER_PORT] [USER_NAME]");
+    let args: Vec<String> = std::env::args().collect();
+
+    let exec_type = &args[1];
+    let is_host = match exec_type.as_str() {
+        "client" => false,
+        "server" => true,
+        _ => panic!("Invalid argument, first one must be \"client\" or \"server\"."),
+    };
+
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.insert_resource(Lobby::default());
+
+    if is_host {
+        app.add_plugin(RenetServerPlugin);
+        app.insert_resource(new_renet_server());
+        app.add_system(server_update_system.system());
+        app.add_system(server_sync_players.system());
+        app.add_system(move_players_system.system());
+    } else {
+        app.add_plugin(RenetClientPlugin);
+        app.insert_resource(new_renet_client());
+        app.insert_resource(PlayerInput::default());
+        app.add_system(player_input.system());
+        app.add_system(client_send_input.system().with_run_criteria(run_if_client_conected));
+        app.add_system(client_sync_players.system().with_run_criteria(run_if_client_conected));
+    }
+
+    app.add_startup_system(setup);
+
+    app.run();
+}
+
+fn server_update_system(
+    mut server_events: EventReader<ServerEvent>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut lobby: ResMut<Lobby>,
+    mut server: ResMut<RenetServer>,
+) {
+    for event in server_events.iter() {
+        match event {
+            ServerEvent::ClientConnected(id, _) => {
+                println!("Player {} connected.", id);
+                // Spawn player cube
+                let player_entity = commands
+                    .spawn_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+                        ..Default::default()
+                    })
+                    .insert(PlayerInput::default())
+                    .insert(Player { id: *id })
+                    .id();
+
+                // We could send an InitState with all the players id and positions for the client
+                // but this is easier to do.
+                for &player_id in lobby.players.keys() {
+                    let message = bincode::serialize(&ServerMessages::PlayerConnected { id: player_id }).unwrap();
+                    server.send_message(*id, 0, message).unwrap();
+                }
+
+                lobby.players.insert(*id, player_entity);
+
+                let message = bincode::serialize(&ServerMessages::PlayerConnected { id: *id }).unwrap();
+                server.broadcast_message(0, message);
+            }
+            ServerEvent::ClientDisconnected(id) => {
+                println!("Player {} disconnected.", id);
+                if let Some(player_entity) = lobby.players.remove(id) {
+                    commands.entity(player_entity).despawn();
+                }
+
+                let message = bincode::serialize(&ServerMessages::PlayerDisconnected { id: *id }).unwrap();
+                server.broadcast_message(0, message);
+            }
+        }
+    }
+
+    for client_id in server.clients_id().into_iter() {
+        while let Some(message) = server.receive_message(client_id, 0) {
+            let player_input: PlayerInput = bincode::deserialize(&message).unwrap();
+            if let Some(player_entity) = lobby.players.get(&client_id) {
+                commands.entity(*player_entity).insert(player_input);
+            }
+        }
+    }
+}
+
+fn server_sync_players(mut server: ResMut<RenetServer>, query: Query<(&Transform, &Player)>) {
+    let mut players: HashMap<u64, [f32; 3]> = HashMap::new();
+    for (transform, player) in query.iter() {
+        players.insert(player.id, transform.translation.into());
+    }
+
+    let sync_message = bincode::serialize(&players).unwrap();
+    server.broadcast_message(1, sync_message);
+}
+
+fn client_sync_players(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut client: ResMut<RenetClient>,
+    mut lobby: ResMut<Lobby>,
+) {
+    while let Some(message) = client.receive_message(0) {
+        let server_message = bincode::deserialize(&message).unwrap();
+        match server_message {
+            ServerMessages::PlayerConnected { id } => {
+                println!("Player {} connected.", id);
+                let player_entity = commands
+                    .spawn_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+                        ..Default::default()
+                    })
+                    .id();
+
+                lobby.players.insert(id, player_entity);
+            }
+            ServerMessages::PlayerDisconnected { id } => {
+                println!("Player {} disconnected.", id);
+                if let Some(player_entity) = lobby.players.remove(&id) {
+                    commands.entity(player_entity).despawn();
+                }
+            }
+        }
+    }
+
+    while let Some(message) = client.receive_message(1) {
+        let players: HashMap<u64, [f32; 3]> = bincode::deserialize(&message).unwrap();
+        for (player_id, translation) in players.iter() {
+            if let Some(player_entity) = lobby.players.get(player_id) {
+                let mut transform = Transform::default();
+                transform.translation = (*translation).into();
+                commands.entity(*player_entity).insert(transform);
+            }
+        }
+    }
+}
+
+/// set up a simple 3D scene
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn player_input(keyboard_input: Res<Input<KeyCode>>, mut player_input: ResMut<PlayerInput>) {
+    player_input.left = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
+    player_input.right = keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
+    player_input.up = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
+    player_input.down = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
+}
+
+fn client_send_input(player_input: Res<PlayerInput>, mut client: ResMut<RenetClient>) {
+    let input_message = bincode::serialize(&*player_input).unwrap();
+
+    client.send_message(0, input_message).unwrap();
+}
+
+fn move_players_system(mut query: Query<(&mut Transform, &PlayerInput)>, time: Res<Time>) {
+    for (mut transform, input) in query.iter_mut() {
+        let x = (input.right as i8 - input.left as i8) as f32;
+        let y = (input.down as i8 - input.up as i8) as f32;
+        transform.translation.x += x * PLAYER_MOVE_SPEED * time.delta().as_secs_f32();
+        transform.translation.z += y * PLAYER_MOVE_SPEED * time.delta().as_secs_f32();
+    }
+}
