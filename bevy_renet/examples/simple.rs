7439fc901cbc7397b4277835a16bd52a8b119b0e
Aceeri <conmcclusk@gmail.com>
2022-06-24 22:09:58 -0700

    Renet: revert https://github.com/lucaspoffo/renet/pull/19 (#21)
    * Revert "Renet: get server address from socket instead of ServerConfig (#19)"
    
    This reverts commit 135dc2d7c5803add12d131faf579efd3ec9f24f6.
    
    * Revert "DemoRoyale: fix creating renet server, no need to use server_addr"
    
    This reverts commit 5d6b84f5a8fef8ab9c9309805cb3057ea3aa7808.

--- bevy_renet/examples/simple.rs
+++ bevy_renet/examples/simple.rs
@@ -53,9 +53,10 @@ fn new_renet_client() -> RenetClient {
 }
 
 fn new_renet_server() -> RenetServer {
-    let socket = UdpSocket::bind("127.0.0.1:5000").unwrap();
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
     let connection_config = RenetConnectionConfig::default();
-    let server_config = ServerConfig::new(64, PROTOCOL_ID, *PRIVATE_KEY);
+    let server_config = ServerConfig::new(64, PROTOCOL_ID, server_addr, *PRIVATE_KEY);
     let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
     RenetServer::new(current_time, server_config, connection_config, socket).unwrap()
 }
