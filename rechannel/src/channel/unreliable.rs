0ffe54d233da4ac2321c35978f4d63eed68bd14f
poffo <ltkpoffo@gmail.com>
2022-07-07 15:53:06 -0300

    Rechannel: split channels into Sender and Receiver
    Now server and clients can have different channels. The code was already divided having no meaning for having the send/receive channel being the same. 
    
    Now you need to a configuration for your server channels and for the client channels, but you have way more control over them. No need have channels that the server/client would never use, or have the client limit be the same as the channels.
    
    ChannelNetworkInfo was removed since we would need to refactor it for it to be usable. Since it was not really being used I decided to cut it and another time revisit it.

--- rechannel/src/channel/unreliable.rs
+++ rechannel/src/channel/unreliable.rs
@@ -1,5 +1,4 @@
 use crate::{
-    channel::Channel,
     error::ChannelError,
     packet::{ChannelPacketData, Payload},
 };
@@ -8,7 +7,7 @@ use std::{collections::VecDeque, time::Duration};
 
 use bytes::Bytes;
 
-use super::ChannelNetworkInfo;
+use super::{ReceiveChannel, SendChannel};
 
 /// Configuration for a unreliable and unordered channel.
 /// Messages sent in this channel will behave like a udp packet,
@@ -29,11 +28,21 @@ pub struct UnreliableChannelConfig {
 }
 
 #[derive(Debug)]
-pub(crate) struct UnreliableChannel {
-    config: UnreliableChannelConfig,
+pub(crate) struct SendUnreliableChannel {
+    channel_id: u8,
+    packet_budget: u64,
+    max_message_size: u64,
+    message_send_queue_size: usize,
     messages_to_send: VecDeque<Bytes>,
+    error: Option<ChannelError>,
+}
+
+#[derive(Debug)]
+pub(crate) struct ReceiveUnreliableChannel {
+    channel_id: u8,
+    max_message_size: u64,
+    message_receive_queue_size: usize,
     messages_received: VecDeque<Payload>,
-    info: ChannelNetworkInfo,
     error: Option<ChannelError>,
 }
 
@@ -49,29 +58,29 @@ impl Default for UnreliableChannelConfig {
     }
 }
 
-impl UnreliableChannel {
+impl SendUnreliableChannel {
     pub fn new(config: UnreliableChannelConfig) -> Self {
-        assert!(config.max_message_size < config.packet_budget);
+        assert!(config.max_message_size <= config.packet_budget);
 
         Self {
+            channel_id: config.channel_id,
+            packet_budget: config.packet_budget,
+            max_message_size: config.max_message_size,
+            message_send_queue_size: config.message_send_queue_size,
             messages_to_send: VecDeque::with_capacity(config.message_send_queue_size),
-            messages_received: VecDeque::with_capacity(config.message_receive_queue_size),
-            config,
-            info: ChannelNetworkInfo::default(),
             error: None,
         }
     }
 }
 
-impl Channel for UnreliableChannel {
+impl SendChannel for SendUnreliableChannel {
     fn get_messages_to_send(&mut self, mut available_bytes: u64, _sequence: u16) -> Option<ChannelPacketData> {
         if self.error.is_some() {
             return None;
         }
 
         let mut messages = vec![];
-
-        available_bytes = available_bytes.min(self.config.packet_budget);
+        available_bytes = available_bytes.min(self.packet_budget);
 
         while let Some(message) = self.messages_to_send.pop_front() {
             let message_size = message.len() as u64;
@@ -80,8 +89,6 @@ impl Channel for UnreliableChannel {
             }
 
             available_bytes -= message_size;
-            self.info.messages_sent += 1;
-            self.info.bytes_sent += message_size;
             messages.push(message.to_vec());
         }
 
@@ -90,80 +97,94 @@ impl Channel for UnreliableChannel {
         }
 
         Some(ChannelPacketData {
-            channel_id: self.config.channel_id,
+            channel_id: self.channel_id,
             messages,
         })
     }
 
-    fn advance_time(&mut self, _duration: Duration) {
-        self.info.reset();
-    }
-
-    fn process_messages(&mut self, mut messages: Vec<Payload>) {
-        if self.error.is_some() {
-            return;
-        }
-
-        while let Some(message) = messages.pop() {
-            if message.len() as u64 > self.config.max_message_size {
-                log::error!(
-                    "Received unreliable message with size above the limit, got {} bytes, expected less than {}",
-                    message.len(),
-                    self.config.max_message_size
-                );
-                self.error = Some(ChannelError::ReceivedMessageAboveMaxSize);
-                return;
-            }
-            if self.messages_received.len() == self.config.message_receive_queue_size {
-                log::warn!(
-                    "Received message was dropped in unreliable channel {}, reached maximum number of messages {}",
-                    self.config.channel_id,
-                    self.config.message_receive_queue_size
-                );
-                return;
-            }
-            self.info.messages_received += 1;
-            self.info.bytes_received += message.len() as u64;
-            self.messages_received.push_back(message);
-        }
-    }
-
     fn process_ack(&mut self, _ack: u16) {}
+
+    fn advance_time(&mut self, _duration: Duration) {}
 
     fn send_message(&mut self, payload: Bytes) {
         if self.error.is_some() {
             return;
         }
 
-        if payload.len() as u64 > self.config.max_message_size {
+        if payload.len() as u64 > self.max_message_size {
             log::error!(
                 "Tried to send unreliable message with size above the limit, got {} bytes, expected less than {}",
                 payload.len(),
-                self.config.max_message_size
+                self.max_message_size
             );
             self.error = Some(ChannelError::SentMessageAboveMaxSize);
             return;
         }
 
-        if self.messages_to_send.len() >= self.config.message_send_queue_size {
+        if self.messages_to_send.len() >= self.message_send_queue_size {
             self.error = Some(ChannelError::SendQueueFull);
-            log::warn!("Unreliable channel {} has reached the maximum queue size", self.config.channel_id);
+            log::warn!("Unreliable channel {} has reached the maximum queue size", self.channel_id);
             return;
         }
 
         self.messages_to_send.push_back(payload);
     }
 
+    fn can_send_message(&self) -> bool {
+        self.messages_to_send.len() < self.message_send_queue_size
+    }
+
+    fn error(&self) -> Option<ChannelError> {
+        self.error
+    }
+}
+
+impl ReceiveUnreliableChannel {
+    pub fn new(config: UnreliableChannelConfig) -> Self {
+        assert!(config.max_message_size <= config.packet_budget);
+
+        Self {
+            channel_id: config.channel_id,
+            max_message_size: config.max_message_size,
+            message_receive_queue_size: config.message_receive_queue_size,
+            messages_received: VecDeque::with_capacity(config.message_receive_queue_size),
+            error: None,
+        }
+    }
+}
+
+impl ReceiveChannel for ReceiveUnreliableChannel {
+    fn process_messages(&mut self, mut messages: Vec<Payload>) {
+        if self.error.is_some() {
+            return;
+        }
+
+        while let Some(message) = messages.pop() {
+            if message.len() as u64 > self.max_message_size {
+                log::error!(
+                    "Received unreliable message with size above the limit, got {} bytes, expected less than {}",
+                    message.len(),
+                    self.max_message_size
+                );
+                self.error = Some(ChannelError::ReceivedMessageAboveMaxSize);
+                return;
+            }
+
+            if self.messages_received.len() == self.message_receive_queue_size {
+                log::warn!(
+                    "Received message was dropped in unreliable channel {}, reached maximum number of messages {}",
+                    self.channel_id,
+                    self.message_receive_queue_size
+                );
+                return;
+            }
+
+            self.messages_received.push_back(message);
+        }
+    }
+
     fn receive_message(&mut self) -> Option<Payload> {
         self.messages_received.pop_front()
-    }
-
-    fn can_send_message(&self) -> bool {
-        self.messages_to_send.len() < self.config.message_send_queue_size
-    }
-
-    fn channel_network_info(&self) -> ChannelNetworkInfo {
-        self.info
     }
 
     fn error(&self) -> Option<ChannelError> {
