1835c930c9addfa996cc512323f351f45ec8d49a
poffo <ltkpoffo@gmail.com>
2024-02-21 21:33:55 -0300

    RenetSteam: make all bevy plugin system public

--- renet_steam/src/bevy.rs
+++ renet_steam/src/bevy.rs
@@ -39,13 +39,13 @@ impl Plugin for SteamServerPlugin {
 }
 
 impl SteamServerPlugin {
-    fn update_system(mut transport: Option<NonSendMut<SteamServerTransport>>, mut server: ResMut<RenetServer>) {
+    pub fn update_system(mut transport: Option<NonSendMut<SteamServerTransport>>, mut server: ResMut<RenetServer>) {
         if let Some(transport) = transport.as_mut() {
             transport.update(&mut server);
         }
     }
 
-    fn send_packets(mut transport: Option<NonSendMut<SteamServerTransport>>, mut server: ResMut<RenetServer>) {
+    pub fn send_packets(mut transport: Option<NonSendMut<SteamServerTransport>>, mut server: ResMut<RenetServer>) {
         if let Some(transport) = transport.as_mut() {
             transport.send_packets(&mut server);
         }
@@ -86,11 +86,11 @@ impl Plugin for SteamClientPlugin {
 }
 
 impl SteamClientPlugin {
-    fn update_system(mut transport: ResMut<SteamClientTransport>, mut client: ResMut<RenetClient>) {
+    pub fn update_system(mut transport: ResMut<SteamClientTransport>, mut client: ResMut<RenetClient>) {
         transport.update(&mut client);
     }
 
-    fn send_packets(
+    pub fn send_packets(
         mut transport: ResMut<SteamClientTransport>,
         mut client: ResMut<RenetClient>,
         mut transport_errors: EventWriter<SteamTransportError>,
@@ -100,7 +100,7 @@ impl SteamClientPlugin {
         }
     }
 
-    fn disconnect_on_exit(exit: EventReader<AppExit>, mut transport: ResMut<SteamClientTransport>) {
+    pub fn disconnect_on_exit(exit: EventReader<AppExit>, mut transport: ResMut<SteamClientTransport>) {
         if !exit.is_empty() {
             transport.disconnect();
         }
