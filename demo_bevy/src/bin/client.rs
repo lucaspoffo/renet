7e8f4f640e83084f640f7c735f368b57c8b3d2ad
FTZ <ltkpoffo@gmail.com>
2024-07-20 13:54:30 -0300

    DemoBevy: fix removed imports

--- demo_bevy/src/bin/client.rs
+++ demo_bevy/src/bin/client.rs
@@ -7,10 +7,11 @@ use bevy::{
 };
 use bevy_egui::{EguiContexts, EguiPlugin};
 use bevy_renet::{
+    client_connected,
     renet::{ClientId, RenetClient},
     RenetClientPlugin,
 };
-use demo_bevy::{setup_level, ClientChannel, NetworkedEntities, PlayerCommand, PlayerInput, ServerChannel, ServerMessages};
+use demo_bevy::{connection_config, setup_level, ClientChannel, NetworkedEntities, PlayerCommand, PlayerInput, ServerChannel, ServerMessages};
 use renet_visualizer::{RenetClientVisualizer, RenetVisualizerStyle};
 use smooth_bevy_cameras::{LookTransform, LookTransformBundle, LookTransformPlugin, Smoother};
 
